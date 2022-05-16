8d1fc9ff9c024cca3143c826d89fd235a7a16efd
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-17 00:24:45 +0300

    array

---
+++ tassk 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace tassk_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random blur = new Random();
+            int a = blur.Next(1, 100);
+            int texmin;
+            do
+            {
+
+                Console.WriteLine("bir reqem daxil edin");
+                texmin = int.Parse(Console.ReadLine());
+                if (texmin == a)
+                {
+                    Console.WriteLine("Tebrik edirik....");
+                }
+                if (texmin < a)
+                {
+                    Console.WriteLine("Yuxari");
+
+                }
+                if (texmin > a)
+                {
+                    Console.WriteLine("Asagi");
+                }
+
+            } while (texmin != a);
+
+
+        }
+    }
+}
